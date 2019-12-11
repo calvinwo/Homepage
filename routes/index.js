ec2aa58c5a2dfa22aacf1879c22f385d43290180
calvinwo <calvincdw@gmail.com>
2019-12-11 13:37:33 -1000

    added day 3

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,21 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { pagetitle: 'Homepage' });
 }).get('/day2', function(req, res, next) {
-  res.render('Day2/page', {pagetitle: 'Pug example'});
-});
+  res.render('Day2/page', {pagetitle: 'Messing around with pug'});
+})
+.get('/day3', function(req, res, next) {
+  var clientIPaddr = null,
+  clientProxy = null;
+  
+  // is client going through a proxy?
+  if (req.headers['via']) { // yes
+    clientIPaddr = req.headers['x-forwarded-for'];
+    clientProxy = req.headers['via'];
+  } else { // no
+    clientIPaddr = req.connection.remoteAddress;
+    clientProxy = "none";
+  }
 
+  res.render('Day3/page', {pagetitle : "Client info", useragent: req.headers['user-agent'], ipaddress :  clientIPaddr, proxy : clientProxy});
+});
 module.exports = router;
