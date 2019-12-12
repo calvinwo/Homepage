6f54981cbf0cb7cbd3ea255407a2d9a1d82e5c36
calvinwo <calvincdw@gmail.com>
2019-12-11 20:02:35 -1000

    changed day 3 code

--- routes/index.js
+++ routes/index.js
@@ -9,16 +9,15 @@ router.get('/', function(req, res, next) {
   res.render('Day2/page', {pagetitle: 'Messing around with pug'});
 })
 .get('/day3', function(req, res, next) {
-  var clientIPaddr = null,
-  clientProxy = null;
-  clientIPaddr = requestIp.getClientIp(req);
-
-  // is client going through a proxy?
-  if (req.headers['via']) { // yes
-    clientProxy = req.headers['via'];
-  } else { // no
-    clientProxy = "none";
-  }
-  res.render('Day3/page', {pagetitle : "Client info", useragent: JSON.stringify(req.headers), ipaddress :  clientIPaddr, proxy : clientProxy});
+  res.render('Day3/page', {
+    pagetitle : "Client info", 
+    headers: JSON.stringify(req.headers), 
+    host : req.headers['host'] ? req.headers['host'] : "none",
+    useragent: req.headers['user-agent'] ? req.headers['user-agent'] :"none", 
+    ipaddress :  requestIp.getClientIp(req), 
+    language : req.headers['accept-language'] ? req.headers['accept-language'] :"none",
+    encoding : req.headers['accept-encoding'] ? req.headers['accept-encoding'] :"none",
+    referer : req.headers['referer'] ? req.headers['referer'] :"none",
+    proxy : req.headers['via'] ? req.headers['via'] : "none"});
 });
 module.exports = router;
